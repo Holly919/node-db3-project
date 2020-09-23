99eda84dfe6af6a052b350395c3d95be9afb9140
Holly919 <56270260+Holly919@users.noreply.github.com>
2020-09-23 13:41:04 -0400

    mvp and stretch complete

--- server.js
+++ server.js
@@ -7,4 +7,11 @@ const server = express();
 server.use(express.json());
 server.use('/api/schemes', SchemeRouter);
 
+server.get('/', (req, res, next) => {
+    res.status(200).json({api: "You are the wind beneath my wings"})
+  })
+
 module.exports = server;
+
+
+
